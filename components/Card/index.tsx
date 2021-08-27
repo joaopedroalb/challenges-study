9ba44ea9abc89bf07dd6d32e512ac2259acd2f23
Joao Pedro <joaopedroalb.dev@gmail.com>
2021-08-27 15:21:59 -0300

    Update card component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,15 +1,18 @@
 import styles from './index.module.css'
+import Link from 'next/link'
 
-export default function Card(props:{title:string, description:string, urlImg:string}){
+export default function Card(props:{title:string, description:string, urlImg:string, urlPath:string}){
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <img src={props.urlImg} alt=""/>
+        <Link href={props.urlPath}>
+            <div className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <img src={props.urlImg} alt=""/>
+                </div>
+                <div className={styles.infoContainer}>
+                    <h1>{props.title}</h1>
+                    <span>{props.description}</span>
+                </div>
             </div>
-            <div className={styles.infoContainer}>
-                <h1>{props.title}</h1>
-                <span>{props.description}</span>
-            </div>
-        </div>
+        </Link>
     )
 }
