6e73e9a74cc29073c86965bb53808f236b28214b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-09-23 15:14:27 -0300

    Add challenger 3

--- pages/Challenger-3.tsx
+++ pages/Challenger-3.tsx
@@ -1,7 +1,45 @@
+import { CSSProperties, useState, useRef } from "react"
+
 export default function ChallengerThree(){
+    const style1:CSSProperties = {width:"100%"
+                                ,height:"100vh",
+                                display:"flex",
+                                flexDirection:"column",
+                                alignItems:"center",
+                                justifyContent:"flex-start",
+                                backgroundColor:"#8b1da1",
+                                color: "#fff"
+                                }
+    const styleInputAux:CSSProperties = {
+        width: "70px"
+    }
+    const [number,setNumber] = useState<number>(0);
+    const [numberAux,setNumberAux] = useState<number>(1);
+    
+
+    function increase(){
+        setNumber(number+numberAux);
+    }
+    function decrease(){
+        setNumber(number-numberAux);
+    }
+
+    function onChangeAuxValue(changeArgs:number){
+        setNumberAux(changeArgs)
+    }
     return(
-        <div>
-            Challenger 3
+        <div style={style1}>
+            <h1>Counter</h1>
+            <div>Value: {number}</div>
+            <div style={{display:"flex"}}>
+                <button onClick={increase}>+</button>
+                <button onClick={decrease}>-</button>
+            </div>
+            <br/>
+            <div style={{display:"flex",gap:"10px"}}>
+                <span>Change the value in increate/decrease</span>
+                <input type="number" onChange={e=>onChangeAuxValue(e.target.valueAsNumber)} value={numberAux} style={styleInputAux}/>
+            </div>
         </div>
     )
 }
