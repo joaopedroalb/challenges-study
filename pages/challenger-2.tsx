5c2e831d57f5386d5b6b165b4e0b1943a9a9da53
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-09-23 12:52:16 -0300

    Add new challenger in home

---
+++ pages/challenger-2.tsx
@@ -0,0 +1,5 @@
+export default function challengerTwo(){
+    return (
+        <div>Mouse</div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,16 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.cardContainer}>
-      <Card title="Chess Challenge" 
-            description="Challenge to create a chess board interface" 
-            urlImg="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Chess_Board.svg/1024px-Chess_Board.svg.png"
-            urlPath="challenger-1"/>
+        <Card title="Chess Challenge" 
+              description="Challenge to create a chess board interface" 
+              urlImg="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Chess_Board.svg/1024px-Chess_Board.svg.png"
+              urlPath="challenger-1"
+        />
+        <Card title = "Mouse Challenge"
+              description ="Challenge to work with mouses events"
+              urlImg="https://media.istockphoto.com/videos/push-button-hand-icon-on-white-background-cursor-of-computer-mouse-video-id1250308810?s=640x640"
+              urlPath="challenger-2"
+        />
       </div>
     </div>
   )
