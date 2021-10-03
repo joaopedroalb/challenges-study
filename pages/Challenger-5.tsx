eb9c95c14bba5939c14fe503a2d601f7e3c5ed02
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-03 11:51:13 -0300

    Add home in challenger 5

--- pages/Challenger-5.tsx
+++ pages/Challenger-5.tsx
@@ -1,5 +1,10 @@
+import Gohome from "../components/Gohome";
+import styles from "../styles/challengerfive.module.css"
+
 export default function ChallengerFive(){
     return(
-        <div>FIVE</div>
+        <div>
+            <Gohome/>
+        </div>
     )
 }
