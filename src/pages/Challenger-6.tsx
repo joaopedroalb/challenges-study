4c11f6c4c562c513bd0d380b63c2c32970e7c9be
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-06 20:19:11 -0300

    Finished challenge six

--- src/pages/Challenger-6.tsx
+++ src/pages/Challenger-6.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import styles from '../../styles/challengersix.module.css'
+
+import Gohome from "../components/Gohome";
+
 export default function ChallengerSix(){
+    const [colors,setColors] = useState<any>(null);
+    const[colorBg,setColorBg] = useState("");
+
+    useEffect(()=>{
+        fetch("http://localhost:3000/api/123")
+            .then(resp => resp.json())
+            .then(setColors)
+    },[])
+
+    function renderColors(){
+        if(colors){
+            return colors?.colors.map((resp:any,i:any)=>{
+                return <div key={i} 
+                            style={{backgroundColor:`${resp}`,flex:'1',cursor:'pointer'}}
+                            onClick={()=>setColorBg(resp)}>
+                        </div>
+            })
+        }
+    }
     return(
-        <div>
-            Six challenge
+        <div className={styles.container} style={{backgroundColor:`${colorBg}`}}>
+            <h1>{colors?.title}</h1>
+            <div className={styles.colorsContainer}>
+                {renderColors()}
+            </div>
+            <Gohome/>
         </div>
     )
 }
--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -2,9 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function Questao(req:NextApiRequest,res:NextApiResponse){
     if(req.method=="GET"){
-        const id = req.query.id
         res.status(200).json({
-            title:'Qual é a sua cor preferida',
+            title:'Escolha uma cor',
             colors:[
                 '#EF476F','#FFD166','#06D6A0','#118AB2',"#073B4C"
             ]
