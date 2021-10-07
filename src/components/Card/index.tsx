067f4e0c1398036942cad28b5e1975c31eccd539
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-07 13:52:51 -0300

    Correcao do html do card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,15 +4,15 @@ import Link from 'next/link'
 export default function Card(props:{title:string, description:string, urlImg:string, urlPath:string}){
     return (
         <Link href={props.urlPath}>
-            <div className={styles.container}>
-                <div className={styles.imageContainer}>
+            <article className={styles.container}>
+                <header className={styles.imageContainer}>
                     <img src={props.urlImg} alt=""/>
-                </div>
+                </header>
                 <div className={styles.infoContainer}>
                     <h1>{props.title}</h1>
                     <span>{props.description}</span>
                 </div>
-            </div>
+            </article>
         </Link>
     )
 }
