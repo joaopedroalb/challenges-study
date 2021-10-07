43105aa0a7ff0922c88019236a140080d8ffc340
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-07 19:44:32 -0300

    Mudanca na estrutura do html dos cards e no style

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,16 +3,17 @@ import Link from 'next/link'
 
 export default function Card(props:{title:string, description:string, urlImg:string, urlPath:string}){
     return (
-        <Link href={props.urlPath}>
             <article className={styles.container}>
-                <header className={styles.imageContainer}>
+                <div className={styles.imageContainer}>
                     <img src={props.urlImg} alt=""/>
-                </header>
-                <div className={styles.infoContainer}>
-                    <h1>{props.title}</h1>
-                    <span>{props.description}</span>
                 </div>
+                <header className={styles.infoContainer}>
+                    <h2>{props.title}</h2>
+                    <h3>{props.description}</h3>
+                </header>
+                <Link href={props.urlPath}>
+                <button>See Challenge</button>
+                </Link>
             </article>
-        </Link>
     )
 }
