5836e50b3d7f06065d427bf4b410162bd9a74449
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-07 20:02:24 -0300

    Melhorando o estilo dos cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,9 +4,7 @@ import Link from 'next/link'
 export default function Card(props:{title:string, description:string, urlImg:string, urlPath:string}){
     return (
             <article className={styles.container}>
-                <div className={styles.imageContainer}>
-                    <img src={props.urlImg} alt=""/>
-                </div>
+                <img src={props.urlImg} alt=""/>
                 <header className={styles.infoContainer}>
                     <h2>{props.title}</h2>
                     <h3>{props.description}</h3>
