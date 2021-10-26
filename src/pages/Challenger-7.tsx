9e2b013587c28c0869775b980b0d7d22059b924a
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-26 08:45:12 -0300

    Create challenge seven

---
+++ src/pages/Challenger-7.tsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/challengerseven.module.css"
+
+export default function ChallengerSeven(){
+    return(
+        <div>
+            Challenge 7
+        </div>
+    )
+}
