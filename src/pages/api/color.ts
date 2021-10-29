f38fd30a10b16477c47f22b16e0f032d833da404
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2021-10-29 17:27:26 -0300

    Update api color

--- src/pages/api/color.ts
+++ src/pages/api/color.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const colorObj = {
+    title:'Escolha uma cor',
+    colors:[
+        '#EF476F','#FFD166','#06D6A0','#118AB2',"#073B4C"
+    ]
+}
+
 export default function Questao(req:NextApiRequest,res:NextApiResponse){
     if(req.method=="GET"){
-        res.status(200).json({
-            title:'Escolha uma cor',
-            colors:[
-                '#EF476F','#FFD166','#06D6A0','#118AB2',"#073B4C"
-            ]
-        })
+        res.status(200).json(colorObj)
     }else{
         res.status(405).send('Error')
     }
